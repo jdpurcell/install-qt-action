c8c18002b3537d9ea93dc528c50dc406463d9139
J.D. Purcell <jdpurcell@gmail.com>
2025-04-08 23:25:03 -0400

    Fix bdist_wheel error

--- action/src/main.ts
+++ action/src/main.ts
@@ -408,7 +408,11 @@ const run = async (): Promise<void> => {
 
     if (!inputs.useNaqt || inputs.src || inputs.doc || inputs.example || inputs.tools.length) {
       // Install dependencies via pip
-      await execPython("pip install", ["setuptools", "wheel", `"py7zr${inputs.py7zrVersion}"`]);
+      await execPython("pip install", [
+        "setuptools>=70.1.0",
+        "wheel",
+        `"py7zr${inputs.py7zrVersion}"`,
+      ]);
 
       // Install aqtinstall separately: allows aqtinstall to override py7zr if required
       if (inputs.aqtSource.length > 0) {
